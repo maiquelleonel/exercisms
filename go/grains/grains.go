db4adce0c30a696405a5f87da7af72097ccd50a6
Maiquel Leonel <skywishmtfk@gmail.com>
2024-03-21 17:46:00 -0300

    solve grains

---
+++ go/grains/grains.go
@@ -0,0 +1,19 @@
+package grains
+
+import (
+	"errors"
+	"math"
+)
+
+func Square(number int) (uint64, error) {
+	if number <= 0 || number > 64 {
+		return 0, errors.New("invalid number")
+	}
+
+	return uint64(math.Pow(2, float64(number)-1)), nil
+}
+
+func Total() uint64 {
+	t, _ := Square(65)
+	return t - 1
+}
