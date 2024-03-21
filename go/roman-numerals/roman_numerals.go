0c6d6a636f68320f88dbd27ebcbb7b0c96fb1a86
Maiquel Leonel <skywishmtfk@gmail.com>
2024-03-21 20:49:48 -0300

    functional approach

--- go/roman-numerals/roman_numerals.go
+++ go/roman-numerals/roman_numerals.go
@@ -1,24 +1,51 @@
-package romannumerals
+package romanpattern
 
 import (
 	"errors"
+	"math"
+	"strings"
 )
 
 func ToRomanNumeral(input int) (string, error) {
 	if input <= 0 || input > 3999 {
 		return "", errors.New("Invalid")
 	}
+	var div, rem int
+	var ret, suffix string
 
-	roman_numbers := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
-	roman_letters := []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
-	current := input
-	ret := ""
-	for i, num := range roman_numbers {
-		for current >= num {
-			ret += roman_letters[i]
-			current -= num
-		}
+	switch {
+	case input < 10:
+		ret = pattern(input, "V", "I", "X")
+	case input < 100:
+		div = input / 10
+		rem = int(math.Mod(float64(input), 10))
+		suffix, _ = ToRomanNumeral(rem)
+		ret = pattern(div, "L", "X", "C") + suffix
+	case input < 1_000:
+		div = input / 100
+		rem = int(math.Mod(float64(input), 100))
+		suffix, _ = ToRomanNumeral(rem)
+		ret = pattern(div, "D", "C", "M") + suffix
+	case input < 4_000:
+		div = input / 1_000
+		rem = int(math.Mod(float64(input), 1_000))
+		suffix, _ = ToRomanNumeral(rem)
+		ret = pattern(div, "C", "M", "") + suffix
 	}
 
 	return ret, nil
+}
+
+func pattern(num int, middle string, mod string, next string) string {
+	switch {
+	case num <= 3:
+		return strings.Repeat(mod, num)
+	case num == 4:
+		return mod + middle
+	case num <= 8:
+		return middle + strings.Repeat(mod, num-5)
+	case num == 9:
+		return mod + next
+	}
+	return ""
 }
