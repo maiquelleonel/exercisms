ea2b2e3a9220d760c940fa1a54df433f280c0750
Maiquel Leonel <skywishmtfk@gmail.com>
2024-03-22 18:53:50 -0300

    cleanup

--- go/isbn-verifier/isbn_verifier.go
+++ go/isbn-verifier/isbn_verifier.go
@@ -6,27 +6,18 @@ import (
 )
 
 func IsValidISBN(isbn string) bool {
-
-	if len(isbn) == 0 {
-		return false
-	}
 	re := regexp.MustCompile(`^\d-?\d{3}-?\d{5}-?[0-9X]$`)
 	if !re.MatchString(isbn) {
 		return false
 	}
 
 	isbn = strings.Replace(isbn, "-", "", 3)
-
-	if len(isbn) != 10 {
-		return false
-	}
-	factors := []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
 	var sum int
-	for i, num := range isbn {
-		if num == 'X' {
+	for i, n := range isbn {
+		if n == 'X' {
 			sum += 10
 		} else {
-			sum += int(num-'0') * factors[i]
+			sum += int(n-'0') * (10 - i)
 		}
 	}
 
