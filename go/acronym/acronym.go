9849d179efaa3e0683633eb3f89efcab3dc738ad
Maiquel Leonel <skywishmtfk@gmail.com>
2024-04-03 21:40:07 -0300

    improve performace

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -6,28 +6,17 @@
 package acronym
 
 import (
-	"fmt"
 	"regexp"
 	"strings"
-	"unicode"
 )
+
+var re = regexp.MustCompile(`\s_?|-`)
 
 // Abbreviate should have a comment documenting it.
 func Abbreviate(s string) string {
-	re := regexp.MustCompile(`\s_?|\s-?\s?|-`)
-	words := re.Split(s, 15)
+	var words []string = strings.Fields(re.ReplaceAllString(s, " "))
 	var acr string
-	println(fmt.Sprintf("%s", words))
 	for _, word := range words {
-		if len(word) == 0 || unicode.IsSpace(rune(word[0])) {
-			continue
-		}
-
-		if len(word) > 1 && unicode.IsPunct(rune(word[0])) {
-			acr += string(word[1])
-			continue
-		}
-
 		acr += string(word[0])
 	}
 	return strings.ToUpper(acr)
