f382bb8ea44b12fc2db7928f79a9b72bc679056b
Maiquel Leonel <skywishmtfk@gmail.com>
2024-04-03 21:21:39 -0300

    improve regexp

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -2,5 +2,5 @@ import re
 
 
 def abbreviate(words):
-    initials = [word[0:1] for word in re.split("\\W\\W?", words) if word]
+    initials = [word[0:1] for word in re.split(r'\s_?|\s-?\s?|-', words) if word]
     return "".join(initials).upper()
