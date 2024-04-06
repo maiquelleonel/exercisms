bee7243523039359e62cb1aaa33e38b60335c12b
Maiquel Leonel <skywishmtfk@gmail.com>
2024-04-06 14:05:12 -0300

    better mutex

--- go/bank-account/bank_account.go
+++ go/bank-account/bank_account.go
@@ -4,7 +4,7 @@ import "sync"
 
 // Define the Account type here.
 type Account struct {
-	mu      sync.Mutex
+	mu      *sync.Mutex
 	balance int64
 	isOpen  bool
 }
@@ -13,7 +13,7 @@ func Open(amount int64) *Account {
 	if amount < 0 {
 		return nil
 	}
-	return &Account{balance: amount, isOpen: true}
+	return &Account{balance: amount, isOpen: true, mu: &sync.Mutex{}}
 }
 
 func (a *Account) Balance() (int64, bool) {
@@ -26,11 +26,11 @@ func (a *Account) Balance() (int64, bool) {
 func (a *Account) Deposit(amount int64) (int64, bool) {
 	var ret int64 = 0
 	var ok bool = false
+	a.mu.Lock()
+	defer a.mu.Unlock()
 	if a.isOpen && a.balance > 0 {
-		a.mu.Lock()
 		a.balance += amount
 		ret, ok = a.balance, true
-		a.mu.Unlock()
 	}
 	return ret, ok
 }
@@ -39,12 +39,12 @@ func (a *Account) Close() (int64, bool) {
 	var ret int64 = 0
 	var ok bool = false
 	a.mu.Lock()
+	defer a.mu.Unlock()
 	if a.isOpen {
 		curBalance := a.balance
 		a.balance = 0
 		a.isOpen = false
 		ret, ok = curBalance, true
 	}
-	defer a.mu.Unlock()
 	return ret, ok
 }
