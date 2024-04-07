5ee9bb89f1a0a26bab8fd3b07e884d3d2198a224
Maiquel Leonel <skywishmtfk@gmail.com>
2024-04-07 12:58:11 -0300

    simplify stuff improving performance

--- go/flatten-array/flatten_array.go
+++ go/flatten-array/flatten_array.go
@@ -1,29 +1,22 @@
 package flatten
 
-import (
-	"reflect"
-)
+import "reflect"
 
 func Flatten(nested interface{}) []interface{} {
 	out := []interface{}{}
 	rv := reflect.ValueOf(nested)
-	if rv.Kind() == reflect.Slice {
-		if rv.Len() == 0 {
-			return out
+
+	if rv.Len() == 0 {
+		return out
+	}
+
+	for i := 0; i < rv.Len(); i++ {
+		switch rv.Index(i).Elem().Kind() {
+		case reflect.Slice:
+			out = append(out, Flatten(rv.Index(i).Interface())...)
+		case reflect.Int:
+			out = append(out, rv.Index(i).Interface())
 		}
-		for i := 0; i < rv.Len(); i++ {
-			if rv.Index(i).Interface() == nil {
-				continue
-			}
-
-			if rv.Index(i).Elem().Kind() == reflect.Slice {
-				out = append(out, Flatten(rv.Index(i).Interface())...)
-			} else {
-				out = append(out, rv.Index(i).Interface())
-			}
-
-		}
-
 	}
 
 	return out
